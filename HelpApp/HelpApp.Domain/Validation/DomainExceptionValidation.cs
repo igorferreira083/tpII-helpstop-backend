44d32dbb3cfe5dcdead56dddd20a6cf8c1da2f58
igorferreira083 <igorfefesilva12@gmail.com>
2025-03-10 12:19:13 -0300

    Aula de Estrutura.

---
+++ HelpApp/HelpApp.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelpApp.Domain.Validation
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool haserror, string error)
+        {
+            if (haserror)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+
+        }
+    }
+}
